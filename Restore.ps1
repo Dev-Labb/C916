1ba40dacf9f8021faf64bcbbdd9623c22bc27488
Kerry Daniels <88607552+Dev-Labb@users.noreply.github.com>
2023-03-30 07:51:34 -0700

    Update Restore.ps1

--- Restore.ps1
+++ Restore.ps1
@@ -1,8 +1,13 @@
 # Author: Kerry Daniels Student ID: 001548730
 
+#Creates a new OU called "Fianance"
 New-ADOrganizationalUnit -Path "DC=consultingfirm,DC=com" -Name "Finance" -DisplayName "Finance-Dept" -ProtectedFromAccidentalDeletion $false 
 
+#Imports CSV and sets path to correct OU
 $ADUsers = Import-Csv -path $PSScriptRoot\financePersonnel.csv
+$Path = "OU=Finance,DC=consultingfirm,DC=com"
+
+
 
 foreach ($ADUser in $ADUsers) {
     $FirstName = $ADUser.First_Name  
@@ -22,4 +27,5 @@ foreach ($ADUser in $ADUsers) {
                 -PostalCode $Postal `
                 -OfficePhone $Office `
                 -MobilePhone $Mobile `
+                
 }
